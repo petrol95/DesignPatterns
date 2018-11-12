1e96f8abb02babec9e4b3a4b6769a4e4dbab722f
petrol95 <petrol195@yandex.ru>
2018-11-12 17:14:55 +0300

    created classes ModelDuck & FlyRocketPowered

--- src/Strategy/MiniDuckSimulator.java
+++ src/Strategy/MiniDuckSimulator.java
@@ -3,8 +3,15 @@ package Strategy;
 public class MiniDuckSimulator {
 
     public static void main(String[] args) {
+
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+
+        Duck model = new ModelDuck();
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
+
     }
 }
