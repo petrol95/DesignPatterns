76628f910445a68f6afd88ffaa1177c6ee380de4
petrol95 <petrol195@yandex.ru>
2019-02-05 11:08:23 +0300

    added classes of pkg decorator

--- src/observer/weather/WeatherStation.java
+++ src/observer/weather/WeatherStation.java
@@ -5,8 +5,8 @@ public class WeatherStation {
         WeatherData weatherData = new WeatherData();
 
         CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
-        // StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
-        // ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+        ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
