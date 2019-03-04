820f88dc14a52c0276d268fd5bfd5af4c12412cb
petrol95 <petrol195@yandex.ru>
2019-03-04 11:07:59 +0300

    added pkg iterenum of pkg adapter

--- src/adapter/ducks/TurkeyAdapter.java
+++ src/adapter/ducks/TurkeyAdapter.java
@@ -12,7 +12,7 @@ public class TurkeyAdapter implements Duck {
 	}
   
 	public void fly() {
-		for(int i=0; i < 5; i++) {
+		for(int i = 0; i < 5; i++) {
 			turkey.fly();
 		}
 	}
--- src/adapter/ducks/TurkeyTestDrive.java
+++ src/adapter/ducks/TurkeyTestDrive.java
@@ -5,7 +5,7 @@ public class TurkeyTestDrive {
 		MallardDuck duck = new MallardDuck();
 		Turkey duckAdapter = new DuckAdapter(duck);
  
-		for(int i=0;i<10;i++) {
+		for(int i = 0; i < 10; i++) {
 			System.out.println("The DuckAdapter says...");
 			duckAdapter.gobble();
 			duckAdapter.fly();
